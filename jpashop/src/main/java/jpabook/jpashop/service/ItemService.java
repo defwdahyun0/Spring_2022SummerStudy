5e0722dfdba0133a6dce9fedd21a20f85d0f01c7
defwdahyun0 <wdh112139@gmail.com>
2022-07-20 15:42:25 +0900

    Refactor: merge to change detection

--- jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,17 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    // merge보다 변경감지의 방법 쓰는 것이 안전. Merge는 데이터가 null이 될 가능성이 높다.
+    @Transactional
+    public Item updateItem(Long itemId, String name, int price, int stockQuantity){
+        // 단발성 업데이트하면 안된다.
+        // findItem.change(price, name, stockQunatity); // 의미있는 데이터 찾기
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItems() {
---
+++ jpashop/src/main/java/jpabook/jpashop/service/UpdateItemDto.java
@@ -0,0 +1,10 @@
+package jpabook.jpashop.service;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class UpdateItemDto {
+    // 이런 식으로 업데이트도 가능하다.
+    // parameter
+}
